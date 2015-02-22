0fbaf1dd7608479546032e97f3706ec203115e40
raiben <raiben@gmail.com>
2015-02-22 12:35:58 +0100

    Starting the new version for the new paper with real TDD approach and clean code basis.Related to #9. Test passed: 20/20 Coverage: 90,38%

---
+++ madetools/core/src/main/java/com/velonuboso/made/core/AntRoach.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2015 Rubén Héctor García (raiben@gmail.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.velonuboso.made.core;
+
+import com.velonuboso.made.interfaces.ICharacter;
+import com.velonuboso.made.interfaces.IFactsWriter;
+
+/**
+ *
+ * @author Rubén Héctor García (raiben@gmail.com)
+ */
+public class AntRoach implements ICharacter{
+
+    private String name;
+    private Integer id;
+    private IFactsWriter factsWriter;
+    
+    public AntRoach() {
+        name = "Kroo";
+        id = null;
+        factsWriter = null;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    @Override
+    public void setFactsWriter(IFactsWriter newFactsWriter) {
+        this.factsWriter = newFactsWriter;
+    }
+
+    @Override
+    public void isAdded() {
+        factsWriter.add(null);
+    }
+}
---
+++ madetools/core/src/test/java/made/velonuboso/made/core/AntRoachTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2015 rhgarcia
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package made.velonuboso.made.core;
+
+import com.velonuboso.made.core.AntRoach;
+import com.velonuboso.made.interfaces.IWorld;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+/**
+ *
+ * @author rhgarcia
+ */
+public class AntRoachTest {
+    
+    private AntRoach character;
+    
+    public AntRoachTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        character = new AntRoach();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void UT_GetName_must_return_the_default_name_when_not_set(){
+        assertEquals("Default name for an antroach must be Kroo", "Kroo", character.getName());
+    }
+    
+    @Test
+    public void UT_GetName_must_return_the_name_of_the_agent_when_set(){
+        String expectedName = "Aaaa";
+        
+        character.setName(expectedName);
+        assertEquals("Name could not be set to "+expectedName, expectedName, character.getName());
+    }
+    
+}
