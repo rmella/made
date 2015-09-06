19a09ad24527cef075f4e53a7cb8d0380ca11a50
raiben <raiben@gmail.com>
2015-09-06 23:42:05 +0200

    #27 - Putting correct author in headers

--- madetools/core/src/test/java/com/velonuboso/made/core/abm/unittest/WorldTest.java
+++ madetools/core/src/test/java/com/velonuboso/made/core/abm/unittest/WorldTest.java
@@ -1,7 +1,5 @@
-package com.velonuboso.made.core.abm.unittest;
-
 /*
- * Copyright (C) 2015 rhgarcia
+ * Copyright (C) 2015 Rubén Héctor García (raiben@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,6 +14,8 @@ package com.velonuboso.made.core.abm.unittest;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+package com.velonuboso.made.core.abm.unittest;
 
 import com.velonuboso.made.core.abm.implementation.piece.Piece;
 import com.velonuboso.made.core.abm.implementation.World;
