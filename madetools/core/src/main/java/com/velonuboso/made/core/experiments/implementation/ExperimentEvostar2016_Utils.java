99d43bc0be7d925936a83f315f2cdbb3e91ea98c
Rubén H. García <raiben@gmail.com>
2015-11-01 10:47:30 +0100

    Experiment Evostar2016_Utils: clearing output and refactoring

--- madetools/core/src/main/java/com/velonuboso/made/core/experiments/implementation/ExperimentEvostar2016_Utils.java
+++ madetools/core/src/main/java/com/velonuboso/made/core/experiments/implementation/ExperimentEvostar2016_Utils.java
@@ -38,7 +38,6 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.poi.util.TempFile;
 
 /**
  *
@@ -47,7 +46,7 @@ import org.apache.poi.util.TempFile;
 public class ExperimentEvostar2016_Utils extends BaseExperiment {
 
     private static final String ARGUMENT_EXPORT_BEHAVIOUR_TREE = "exportBehaviourTree";
-    private static final String DEFAULT_EXTENSION = "jpg";
+    final String EXECUTABLE = "dot";
 
     public ExperimentEvostar2016_Utils() {
     }
@@ -69,63 +68,99 @@ public class ExperimentEvostar2016_Utils extends BaseExperiment {
             exportBehaviourTree(outputFileName);
             System.exit(0);
         }
-
         printHelp(parser);
     }
 
     private void exportBehaviourTree(String outputFileName) {
-        final String EXECUTABLE = "dot";
+        final String DOT = ".";
+        final String DEFAULT_EXTENSION = "jpg";
 
         checkExecutableExists(EXECUTABLE);
 
-        ICharacter character = ObjectFactory.createObject(ICharacter.class);
-
-        float[] chromosome = new float[50];
-        Arrays.fill(chromosome, 0.5f);
-        AbmConfigurationEntity entity = new AbmConfigurationEntity(chromosome);
-        character.setAbmConfiguration(entity);
-
-        IBehaviourTreeNode node = character.getBehaviourTree();
+        IBehaviourTreeNode node = getDefaultBehaviourTree();
         String graph = getBehaviourTreeAsDigraph(node);
+
         try {
-            File digraphTemporalFile = File.createTempFile("made_", "_bt.tmp");
-            Files.write(Paths.get(digraphTemporalFile.toURI()), graph.getBytes(), StandardOpenOption.WRITE);
-
-            if (!outputFileName.contains(".")){
-                outputFileName = outputFileName+"."+ DEFAULT_EXTENSION;
+            File digraphTemporalFile = saveDotToTemporalFile(graph);
+            if (!outputFileName.contains(DOT)) {
+                outputFileName = outputFileName + DOT + DEFAULT_EXTENSION;
             }
-            String extension = getExtensionFromFileName (outputFileName);
-            
-            Process dotExecutionProcess = Runtime.getRuntime().exec(EXECUTABLE
-                    + " -T"+extension+" " + digraphTemporalFile.getAbsolutePath()
-                    + " -o " + outputFileName);
-
+            Process dotExecutionProcess = runDot(digraphTemporalFile, outputFileName);
             redirectToSystemOutput(dotExecutionProcess.getInputStream());
             redirectToSystemOutput(dotExecutionProcess.getErrorStream());
 
             digraphTemporalFile.delete();
+
+            showMessageSuccess(outputFileName);
         } catch (Exception e) {
             Logger.getLogger(ExperimentEvostar2016_Utils.class.getName()).log(Level.SEVERE, null, e.getMessage());
         }
     }
 
+    private void checkExecutableExists(String executableName) {
+        boolean existsInPath = Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator)))
+                .map(Paths::get)
+                .anyMatch(path -> Files.exists(path.resolve(executableName)) || Files.exists(path.resolve(executableName + ".exe")));
+        if (!existsInPath) {
+            Logger.getLogger(ExperimentEvostar2016_Utils.class.getName()).log(Level.SEVERE,
+                    "Could not find " + executableName + " in the path");
+        }
+    }
+
+    private IBehaviourTreeNode getDefaultBehaviourTree() {
+        ICharacter character = ObjectFactory.createObject(ICharacter.class);
+        configureCharacter(character);
+        IBehaviourTreeNode node = character.getBehaviourTree();
+        return node;
+    }
+
+    private void showMessageSuccess(String outputFileName) {
+        String messageSuccess = "Output file successfully created: " + outputFileName;
+        System.out.println(messageSuccess);
+    }
+
+    private Process runDot(File digraphTemporalFile, String outputFileName) throws IOException {
+        String extension = getExtensionFromFileName(outputFileName);
+        Process dotExecutionProcess = Runtime.getRuntime().exec(EXECUTABLE
+                + " -T" + extension + " " + digraphTemporalFile.getAbsolutePath()
+                + " -o " + outputFileName);
+        return dotExecutionProcess;
+    }
+
+    private void configureCharacter(ICharacter character) {
+        float[] chromosome = new float[50];
+        Arrays.fill(chromosome, 0.5f);
+        AbmConfigurationEntity entity = new AbmConfigurationEntity(chromosome);
+        character.setAbmConfiguration(entity);
+    }
+
+    private File saveDotToTemporalFile(String graph) throws IOException {
+        File digraphTemporalFile = File.createTempFile("made_", "_bt.tmp");
+        Files.write(Paths.get(digraphTemporalFile.toURI()), graph.getBytes(), StandardOpenOption.WRITE);
+        return digraphTemporalFile;
+    }
+
     private void redirectToSystemOutput(InputStream stream) throws IOException {
+        final String LINE_SEPARATOR_SYSTEM_PROPERTY = "line.separator";
+        
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         StringBuilder builder = new StringBuilder();
         String line = null;
         while ((line = reader.readLine()) != null) {
             builder.append(line);
-            builder.append(System.getProperty("line.separator"));
+            builder.append(System.getProperty(LINE_SEPARATOR_SYSTEM_PROPERTY));
         }
         String result = builder.toString();
 
-        Logger.getLogger(ExperimentEvostar2016_Utils.class.getName()).log(Level.INFO, result);
+        if (!StringUtils.isBlank(result)) {
+            Logger.getLogger(ExperimentEvostar2016_Utils.class.getName()).log(Level.INFO, result);
+        }
     }
 
     private OptionParser buildOptionParser() {
         OptionParser parser = new OptionParser();
-        parser.accepts(ARGUMENT_EXPORT_BEHAVIOUR_TREE, "Export behaviour tree to pdf in "
-                + "the specified file (pdf extension added automatically)").withRequiredArg().ofType(String.class);
+        parser.accepts(ARGUMENT_EXPORT_BEHAVIOUR_TREE, "Export behaviour tree to "
+                + "the specified file (if none specified, jpg is used)").withRequiredArg().ofType(String.class);
         parser.allowsUnrecognizedOptions();
         return parser;
     }
@@ -150,16 +185,6 @@ public class ExperimentEvostar2016_Utils extends BaseExperiment {
         return builder.toString();
     }
 
-    private void checkExecutableExists(String executableName) {
-        boolean existsInPath = Stream.of(System.getenv("PATH").split(Pattern.quote(File.pathSeparator)))
-                .map(Paths::get)
-                .anyMatch(path -> Files.exists(path.resolve(executableName)) || Files.exists(path.resolve(executableName + ".exe")));
-        if (!existsInPath) {
-            Logger.getLogger(ExperimentEvostar2016_Utils.class.getName()).log(Level.SEVERE,
-                    "Could not find " + executableName + " in the path");
-        }
-    }
-
     private void recursivePrintChildren(IBehaviourTreeNode parent, StringBuilder builder) {
         if (parent.getChildren() == null) {
             return;
@@ -172,15 +197,19 @@ public class ExperimentEvostar2016_Utils extends BaseExperiment {
     }
 
     private void printChild(IBehaviourTreeNode parent, IBehaviourTreeNode child, StringBuilder builder) {
+        final String SUBSTRING_IN_CONDITION = "Condition";
+        final String SUBSTRING_PRESENT_IN_LAMBDA_NODE_NAME = "lambda";
+        final String BASE_NODE_NAME = "Root node";
+
         String parentName = getNodeName(parent);
         String parentLabel = convertClassNameToReadableFormat(parent.getAction().getClass().getSimpleName());
-        if (parentLabel.contains("lambda")) {
-            parentLabel = "Root node";
+        if (parentLabel.contains(SUBSTRING_PRESENT_IN_LAMBDA_NODE_NAME)) {
+            parentLabel = BASE_NODE_NAME;
             builder.append("node_" + parentName + " [shape=circle];\n");
         }
 
         String childClass = child.getAction().getClass().getSimpleName();
-        boolean isCondition = childClass.contains("Condition");
+        boolean isCondition = childClass.contains(SUBSTRING_IN_CONDITION);
 
         String childLabel = convertClassNameToReadableFormat(childClass);
         String childName = getNodeName(child);
@@ -193,27 +222,33 @@ public class ExperimentEvostar2016_Utils extends BaseExperiment {
                 + "-> {node_" + childName + " [label=\"" + childLabel + "\"]};\n");
     }
 
-    static String convertClassNameToReadableFormat(String s) {
-        String finalText = s.replaceAll(
-                String.format("%s|%s|%s",
-                        "(?<=[A-Z])(?=[A-Z][a-z])",
-                        "(?<=[^A-Z])(?=[A-Z])",
-                        "(?<=[A-Za-z])(?=[^A-Za-z])"
-                ),
-                " "
-        );
-        finalText = finalText.replaceAll("(Condition |Strategy )", "");
-        finalText = finalText.toLowerCase();
-        finalText = finalText.replace("friend similarity", "friend's similarity");
-        finalText = finalText.replace("enemy similarity", "enemy's similarity");
-        finalText = finalText.replace(" ", "\n");
-        return finalText;
-    }
-
     private void includeGeneralPropertieForGraph(StringBuilder builder) {
         builder.append("graph [pad=\".2\", ranksep=\"0.5\", nodesep=\"0.25\", rankdir=LR, ordering=out, splines=ortho];\n"
                 + "node [fontname=\"FreeSans\",fontsize=\"16\",shape=box,width=1.1, height=1.1 margin=0.1, style=rounded];\n"
                 + "edge [fontname=\"FreeSans\",fontsize=\"12\",labelfontname=\"FreeSans\",labelfontsize=\"10\"]\n;");
+    }
+
+    static String convertClassNameToReadableFormat(String text) {
+        String finalText = splitByCamelCase(text);
+        finalText = removePrefix(finalText);
+        finalText = finalText.toLowerCase();
+        finalText = addGenitives(finalText);
+        finalText = convertToMultiLine(finalText);
+        return finalText;
+    }
+
+    private static String removePrefix(String finalText) {
+        return finalText.replaceAll("(Condition |Strategy )", "");
+    }
+
+    private static String convertToMultiLine(String finalText) {
+        return finalText.replace(" ", "\n");
+    }
+
+    private static String addGenitives(String finalText) {
+        finalText = finalText.replace("friend similarity", "friend's similarity");
+        finalText = finalText.replace("enemy similarity", "enemy's similarity");
+        return finalText;
     }
 
     private String getNodeName(IBehaviourTreeNode parent) {
@@ -222,5 +257,16 @@ public class ExperimentEvostar2016_Utils extends BaseExperiment {
 
     private String getExtensionFromFileName(String outputFileName) {
         return FilenameUtils.getExtension(outputFileName).toLowerCase();
+    }
+
+    private static String splitByCamelCase(String text) {
+        return text.replaceAll(
+                String.format("%s|%s|%s",
+                        "(?<=[A-Z])(?=[A-Z][a-z])",
+                        "(?<=[^A-Z])(?=[A-Z])",
+                        "(?<=[A-Za-z])(?=[^A-Za-z])"
+                ),
+                " "
+        );
     }
 }
